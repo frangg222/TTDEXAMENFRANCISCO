1c20f0ee57ba3fd0f7b5885a312464f50c1ad78a
francisco <correo@gmail.com>
2023-06-06 18:48:57 +0200

    TTD TERMINAD Y COMENTADO

--- Test/TestTMoto.java
+++ Test/TestTMoto.java
@@ -10,10 +10,71 @@ public class TestTMoto {
     public void Test_CrearTMoto(){
     TMoto nuevaTMoto=new TMoto();
         /**
+         * Añado valores erroneos para futura AutoCorreccion
+         */
+    nuevaTMoto.setRuedas(0);
+    nuevaTMoto.setCilindrada(0);
+    nuevaTMoto.setMarchas(-1);
+    nuevaTMoto.setVelocidad(-5);
+
+
+
+        /**
          *Creo el atributo velociad y al ser privado creo el getter y el setter
          */
         Assertions.assertEquals(0,nuevaTMoto.getVelocidad());
+        /**
+         * Creo el atributo ruedas y sus getter y setters
+         */
+        Assertions.assertEquals(2,nuevaTMoto.getRuedas());
+        /**
+         * Creo el atributo marcha y sus getter y setters
+         */
+        Assertions.assertEquals(0,nuevaTMoto.getMarchas());
+        /**
+         *  Creo el atributo Cilindrada y los getter y setters
+         */
+        Assertions.assertEquals(49,nuevaTMoto.getCilindrada());
 
     }
+
+    @Test
+    public void Test_Acelero(){
+        /**
+         * Al ACELERAR HAY QUE SUBIR MARCHAR Y VELOCIDAD ASIQUE AL CREAR EL TEST ME PIDE CREAR LOS METODOS
+         */
+        TMoto nuevaTMoto=new TMoto();
+        nuevaTMoto.subirmarcha();
+        nuevaTMoto.aceleracion(25);
+        /**
+         * Despues de crear el metodo Pruebo que funcione correctamente INCLUSO TENIENDO EN MENTE LOS CONDICIONALES DE LOS SETTERS
+         */
+        Assertions.assertEquals(1,nuevaTMoto.getMarchas());
+        Assertions.assertEquals(25,nuevaTMoto.getVelocidad());
+
+
+
+    }
+    @Test
+    public void Test_Decelero(){
+        /**
+         * Al DECELERAR HAY QUE BAJAR MARCHAS Y VELOCIDAD ASIQUE AL CREAR EL TEST ME PIDE CREAR LOS METODOS
+         */
+        TMoto nuevaTMoto=new TMoto();
+        nuevaTMoto.aceleracion(50);
+        nuevaTMoto.subirmarcha();
+        nuevaTMoto.subirmarcha();
+        nuevaTMoto.bajarmarcha();
+        nuevaTMoto.decelerar(30);
+        /**
+         * COMPRUEBO QUE LA MARCHA SE QUEDA EN 1 Y QUE LA VELOCIDAD BAJA A 20
+         */
+        Assertions.assertEquals(1,nuevaTMoto.getMarchas());
+        Assertions.assertEquals(20,nuevaTMoto.getVelocidad());
+    }
+
+    /**
+     * Al pasar los Test REFACTORIZO LO NECESARIO
+     */
 
 }
--- src/TMoto.java
+++ src/TMoto.java
@@ -1,12 +1,111 @@
 public class TMoto {
+    public int getCilindrada() {
+        return Cilindrada;
+    }
+
+    public void setCilindrada(int cilindrada) {
+        Cilindrada = cilindrada;
+        /**
+         * AÑADO CONDICIONALES DESPUES DE PASAR EL PRIMER TEST PARA ARREGLAR POSBLES ERRORES COMO MENOR O MAYO CILINDRADA Y ASI PODER VOLVER A PASARLO
+         */
+        if (Cilindrada <=49){
+            Cilindrada=49;
+        } else if (Cilindrada>=1200) {
+            Cilindrada=1200;
+
+        }
+    }
+
+    private int Cilindrada;
+
+    private int Marchas;
+    public int getRuedas() {
+        return Ruedas;
+    }
+
+    public void setRuedas(int ruedas) {
+        Ruedas = ruedas;
+        /**
+         * AÑADO CONDICIONALES DESPUES DE PASAR EL PRIMER TEST PARA ARREGLAR POSBLES ERRORES COMO NO HAY RUEDAS O HAY DEMASIADAS Y ASI PODER VOLVER A PASARLO
+         */
+        if (Ruedas <=2){
+            Ruedas=2;
+        } else if (Ruedas>=4) {
+            Ruedas=4;
+
+        }
+    }
+
+    private int Ruedas;
+
+
     public int getVelocidad() {
-        return velocidad;
+        return Velocidad;
     }
 
     public void setVelocidad(int velocidad) {
-        this.velocidad = velocidad;
+        this.Velocidad = velocidad;
+        /**
+         * AÑADO CONDICIONALES DESPUES DE PASAR EL PRIMER TEST PARA ARREGLAR POSBLES ERRORES COMO MENOR O MAYOR VELOCIDAD DE LA ESPERADA Y ASI PODER VOLVER A PASARLO
+         */
+            if (Velocidad <=0){
+                Velocidad=0;
+            } else if (Velocidad >=230) {
+                Velocidad=230;
+            }
     }
 
-    private int velocidad;
+    private int Velocidad;
 
+    public int getMarchas() {
+        return Marchas;
+    }
+
+    public void setMarchas(int marchas) {
+        this.Marchas = marchas;
+        /**
+         * AÑADO CONDICIONALES DESPUES DE PASAR EL PRIMER TEST PARA ARREGLAR POSBLES ERRORES COMO QUE NO HAY MARCHAS O NO EXISTEN Y ASI PODER VOLVER A PASARLO
+         */
+        if(Marchas <=0){
+            Marchas=0;
+        } else if (Marchas>=6) {
+            Marchas=6;
+        }
+    }
+
+    public void subirmarcha() {
+        /**
+         * CREO EL METODO PARA SUBIR MARCHAS YA QUE EL TEST ME LO PIDE
+         */
+        int lamarcha;
+        lamarcha=Marchas+1;
+        setMarchas(lamarcha);
+    }
+
+    public void aceleracion(int Kmh) {
+        /**
+         * CREO EL METODO PARA ACELERAR YA QUE EL TEST ME LO PIDE
+         */
+        int velmas;
+        velmas=Kmh+Velocidad;
+        setVelocidad(velmas);
+    }
+
+    public void bajarmarcha() {
+        /**
+         * CREO EL METODO PARA BAJAR MARCHAS YA QUE EL TEST ME LO PIDE
+         */
+        int lamarcha;
+        lamarcha=Marchas-1;
+        setMarchas(lamarcha);
+    }
+
+    public void decelerar(int Kmh) {
+        /**
+         * CREO EL METODO PARA DECELERAR YA QUE EL TEST ME LO PIDE
+         */
+        int velmas;
+        velmas=Velocidad-Kmh;
+        setVelocidad(velmas);
+    }
 }
